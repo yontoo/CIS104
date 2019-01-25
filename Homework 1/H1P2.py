02e17d754712f9e628b45a3c152a43740f35a4a4
Tray Harris <traylee1513@yahoo.com>
2019-01-24 21:35:16 -0800

    Homework 1 Part 2 initial commit

---
+++ Homework 1/H1P2.py
@@ -0,0 +1,3 @@
+miles = float(input("Please enter the amount of miles you would like to convert to kilometers: "))
+conversion = miles * 1.609
+print(str(miles) + " miles is " + str(conversion) + " in kilometers.")
