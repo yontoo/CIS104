6c08e829e89a1483fed540123e96a143ea649be4
Tray Harris <traylee1513@yahoo.com>
2019-02-14 21:25:31 -0800

    Additional progress

---
+++ Homework 3/calculator.py
@@ -0,0 +1,32 @@
+from cmath import exp
+
+current_mem = 0.0
+
+def add(n1, n2):
+    return n1 + n2
+
+def  subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+def power(n1, n2):
+    return exp(n1, n2)
+
+def invert(n1):
+    return -n1
+
+def  call_mem():
+    return current_mem
+
+def save_mem(n1):
+    global current_mem
+    current_mem = n1
+
+def clear_mem():
+    global current_mem
+    current_mem = 0.0
--- Homework 3/main.py
+++ Homework 3/main.py
@@ -1,26 +1,59 @@
-import calculator as calc
+import calculator
 import msvcrt
 
-fval_str = ""
-sval_str = ""
-op = ' '
 result = 0.0
-which_num = 1
-
-print(">")
-
-while(True)
-    curr_key = getch()
-    if(curr_key == '1' | curr_key == '2' | curr_key == '3' | curr_key == '4' | curr_key == '5' | curr_key == '6'| curr_key == '7' | curr_key == '8' | curr_key == '9' | curr_key == '0'| curr_key == | '.')
-        print(curr_key)
-
-        if(which_num == 1)
-            fval_str += curr_key
-        else
-            sval_str =+ curr_key
-        break
-        
 
 
-    if(curr_key == 'X' or curr_key == 'x')
-        break
+def main():
+    fval = 0
+    sval = 0
+    fval_str = ""
+    sval_str = ""
+    op = ' '
+
+    which_num = 1
+    curr_key = ''
+    print(">")
+    while(True):
+        curr_key = msvcrt.getch()
+        if(curr_key == '1' or curr_key == '2' or curr_key == '3' or curr_key == '4' or curr_key == '5' or curr_key == '6' or curr_key == '7' or curr_key == '8' or curr_key == '9' or curr_key == '0'or curr_key == '.'):
+            print(curr_key)
+
+            if(which_num == 1):
+                fval_str += curr_key
+            else:
+                sval_str += curr_key
+            break
+        if(curr_key == '+' or curr_key == '-' or curr_key == '/' or curr_key == '*' or curr_key == '^'):  
+            if (which_num == 1):
+                fval = float(fval_str)
+                which_num = 2
+                fval_str = ""
+                sval_str = ""
+            op = curr_key
+            print(curr_key)
+            break
+        if(curr_key == chr(13)):
+            print("\n")
+            if (sval_str != ""):
+                sval = float(sval_str)
+                if (op == '+'):
+                    global result
+                    result = calculator.add(fval, sval)
+                    break
+                elif(op == '-'):
+                    result = calculator.subtract(fval, sval)
+                    break
+                elif(op == '/'):
+                    result = calculator.divide(fval, sval)
+                    break
+                elif(op == '*'):
+                    result = calculator.multiply(fval, sval)
+                    break
+                elif(op == '^'):
+                    result = calculator.power(fval, sval)
+                    break
+
+
+        if(curr_key == 'X' or curr_key == 'x'):
+            break
