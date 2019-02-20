043d38eedd353184e87fc9ca124882d4be4c937e
Tray Harris <traylee1513@yahoo.com>
2019-02-19 17:57:07 -0800

    Added more comments.

--- Homework 3/calculator.py
+++ Homework 3/calculator.py
@@ -11,22 +11,28 @@ def  subtract(n1, n2):
 def multiply(n1, n2):
     return n1 * n2
 
+#Function to divide 2 inputed numbers.
 def divide(n1, n2):
     return n1 / n2
 
+#Function to get the result of n1 to the power of n2.
 def power(n1, n2):
     return math.pow(n1, n2)
 
+#Inverts the inputed number.
 def invert(n1):
     return n1 * -1
 
+#Returns the value in the varialbe current_mem
 def  call_mem():
     return current_mem
 
+#Assigns the global variable current_mem to the inputed number.
 def save_mem(n1):
     global current_mem
     current_mem = n1
 
+#Assigns the global variable current_mem to 0.
 def clear_mem():
     global current_mem
     current_mem = 0.0
--- Homework 3/main.py
+++ Homework 3/main.py
@@ -10,18 +10,21 @@ sval_str = ""
 print(">", end='', flush = True)
 while(True):
     
-    curr_key = msvcrt.getwch()
+    #getwch returns keypresses as wide characters in unicode, as opposed to getch wich returns a value in byte format.
+    curr_key = msvcrt.getwch()  
+    #Tests if curr_key is 0-9 or ".", if it is, print the keypress and add it to fval string or sval string, depending on if the current number being added to is the first or second one.        
     if(curr_key == '1' or curr_key == '2' or curr_key == '3' or curr_key == '4' or curr_key == '5' or curr_key == '6' or curr_key == '7' or curr_key == '8' or curr_key == '9' or curr_key == '0'or curr_key == '.'):
         print(curr_key, end='', flush = True)
 
-        if(which_num == 1 and expr_done):
+        if(which_num == 1 and expr_done):       #Uses the boolean expr_done to determine whether or not the next expression is new, or a continuation of the last result.
             fval_str = ''
             fval_str += curr_key
         elif(which_num == 1):
             fval_str += curr_key
         else:
             sval_str += curr_key
-
+    #Tests if curr_key is "+", "-", "*", "/" or "^". If it is, assign fval to the float of fvla_string and indicate the current number being worked on is the second one.
+    #Reset the string values to keep it clean and assign the operation to the keypress.
     if(curr_key == '+' or curr_key == '-' or curr_key == '/' or curr_key == '*' or curr_key == '^'):  
         if (which_num == 1):
             fval = float(fval_str)
@@ -31,19 +34,26 @@ while(True):
         op = curr_key
         print(curr_key, end='', flush = True)
     
+    #Clears the stored memory when "m" is cleared.
     if(curr_key == 'm' or curr_key == 'M'):
         calculator.clear_mem()
         print("Memory Cleared")
+        print(">", end='', flush = True)
     
+    #Inverts the current value's sign when "i" is pressed.
     if(curr_key == 'i' or curr_key == 'I'):
         result = calculator.invert(result)
         fval_str = str(result)
         print("Current value inverted. (" + str(result) + ")")
+        print(">", end='', flush = True)
     
+    #Saves current value in the calculator when "s" is pressed.
     if(curr_key == 's' or curr_key == 'S'):
         calculator.save_mem(result)
         print("Value saved!")
+        print(">", end='', flush = True)
     
+    #Resets all values in the calculator when "c" is pressed.
     if(curr_key == 'c' or curr_key == 'C'):
         fval = 0.0
         sval = 0.0
@@ -53,14 +63,17 @@ while(True):
         result = 0.0
         expr_done = False
         print("Calculator cleared.")
+        print(">", end='', flush = True)
         
-
+    #Recalls stored memory when r is pressed.
     if(curr_key == 'r' or curr_key == 'R'):
         result = calculator.call_mem()
         fval_str = str(result)
         print("Stored value is " + str(result) + ".")
+        print(">", end='', flush = True)
 
-        
+    #Tests if Enter was pressed. If it was and if sval_string is not empty, choose a function based on the current operator.
+    #Also assings the fval_str to the string of result, resets sval_str, sets the current number being worked on to 1 and sets the expr_done indicator to True.
     if(curr_key == chr(13)):
         if (sval_str != ""):
             sval = float(sval_str)
@@ -91,6 +104,6 @@ while(True):
 
               
 
-
+    #Exits the program when x is pressed.
     if(curr_key == 'X' or curr_key == 'x'):
         break
