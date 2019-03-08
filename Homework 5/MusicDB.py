4719e63c63996971ed0c0ca659ea7d1388bc5152
Tray Harris <traylee1513@yahoo.com>
2019-03-07 21:39:09 -0800

    Progress.

--- Homework 5/MusicDB.py
+++ Homework 5/MusicDB.py
@@ -1,8 +1,23 @@
 def help():
     print("Commands in this program:")
-    print("1. add - Add a new song to the database.\n")
-    print("2. list - List the songs in the database.\n")
-    print("3. save - Save the songs to the database.\n")
-    print("4. clear - Clear the current songs inside of the database.\n")
-    print("5. help - Display this menu again.\n")
-    print("6. exit - Close program.\n")
+    print("1. add - Add a new song to the database.")
+    print("2. list - List the songs in the database.")
+    print("3. save - Save the songs to the database.")
+    print("4. clear - Clear the current songs inside of the database.")
+    print("5. help - Display this menu again.")
+    print("6. exit - Close program.")
+
+def add():
+    song = input("Enter song title: ")
+    artist = input("Enter artist: ")
+    album = input("Enter album: ")
+    trk_num = int(input("Enter track #: "))
+    song_year = int(input("Enter release year: "))
+    genre = input("Enter genre: ")
+
+#https://python-forum.io/Thread-Looping-to-Create-Nested-Dictionary
+    add_songs = dict(dict_song = song, dict_art = artist, dict_album = album, dict_trknum = trk_num, dict_year = song_year, dict_genre = genre)
+buncha_songs = {
+    "Song1": 
+}
+
